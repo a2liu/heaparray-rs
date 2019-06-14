32cd9d126d71f8707cf3f0a574aa289ae6607fc3
Albert Liu <albertymliu@gmail.com>
2019-06-14 16:58:51 -0400

    added some tests

--- tests/memory_model/mem_block.rs
+++ tests/memory_model/mem_block.rs
@@ -1,1 +1,25 @@
 use crate::prelude::*;
+use heaparray::base::MemBlock;
+
+#[test]
+pub fn ref_no_dealloc() {
+    let blk = unsafe { MemBlock::<Vec<u8>, ()>::alloc(200) };
+    let info = before_alloc();
+    after_alloc(blk, info);
+}
+
+#[test]
+pub fn ref_dealloc_lazy() {
+    let info = before_alloc();
+    let mut blk = unsafe { MemBlock::<Vec<u8>, ()>::alloc(200) };
+    unsafe { blk.as_mut().dealloc_lazy(200) };
+    after_alloc(blk, info);
+}
+
+#[test]
+pub fn ref_dealloc_normal() {
+    let info = before_alloc();
+    let mut blk = MemBlock::<Vec<u8>, ()>::new_init((), 200, |_, _| Vec::with_capacity(10));
+    unsafe { blk.as_mut().dealloc(200) };
+    after_alloc(blk, info);
+}
